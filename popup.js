49c1e77cfc061c0a2b077a7bf2baf0aab59b7d1d
N-Fal <nolanafallin@gmail.com>
2024-02-24 02:16:19 -0800

    init

---
+++ popup.js
@@ -0,0 +1,40 @@
+
+// listens for text input on the popup and passes the card name to submitForm()
+var nameForm = document.getElementById('name_form')
+nameForm.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        // test deleting this
+        event.preventDefault();
+        submitForm();
+        rateLimit(1000);
+        document.getElementById('textInput').value = ""; 
+    }
+})
+
+// processes card name and updates display
+function submitForm()
+{
+    var input = document.getElementById('textInput').value;
+    if (input !== "")
+    {
+        document.getElementById('output').innerHTML = input;
+        console.log("received ", input);
+    }
+}
+
+// prevents user from entering another name for 't' ms
+function rateLimit(t) {
+    console.log("disabling input for ", t, " ms");
+    document.getElementById("textInput").disabled = true;
+
+    setTimeout(function() {
+        document.getElementById("textInput").disabled = false;
+        console.log("input has been enabled");
+    }, t);
+}
+
+function bgColor()
+{
+    var bgId = document.getElementById('background');
+    bgId.style = getCardColor()
+}
